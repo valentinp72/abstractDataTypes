4111b3f4d1a71f318a7c2e55ad48a38d65f66110
Valentin Pelloin <valentinpelloin@gmail.com>
2017-04-02 00:20:54 +0200

    test for labels

--- labels/test.c
+++ labels/test.c
@@ -1,5 +1,41 @@
 #include "labels.h"
 
 int main(){
+	int    aInt     = 1;
+	long   aLong    = 2;
+	float  aFloat   = 1.5;
+	double aDouble  = 2.5;
+	bool   aBool1   = true;
+	bool   aBool2   = false;
+	int *  aPointer = NULL;
+	char   aChar    = 'A';
+	char * aString  = "Hello world!";
+
+	printf("Int\t: ");
+	label_printInt(&aInt);
+
+	printf("\nLong\t: ");
+	label_printLong(&aLong);
+
+	printf("\nFloat\t: ");
+	label_printFloat(&aFloat);
+
+	printf("\nDouble\t: ");
+	label_printDouble(&aDouble);
+
+	printf("\nBool\t: ");
+	label_printBool(&aBool1);
+	printf("\nBool\t: ");
+	label_printBool(&aBool2);
+
+	printf("\nPointer\t: ");
+	label_printPointer(&aPointer);
 	
+	printf("\nChar\t: ");
+	label_printChar(&aChar);
+
+	printf("\nString\t: ");
+	label_printString(aString);
+
+	printf("\n");
 }
